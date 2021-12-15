b09d961c7b02cc6fb2107b5e09ecd22f7e37b233
Jonathan Dempsey <jonathanedempsey@gmail.com>
2021-12-15 17:10:15 +0000

    Types intro and start files

---
+++ playground/types-intro.js
@@ -0,0 +1,9 @@
+// How to remember types - SNOB 'N' US
+
+// String - any sort of text
+// Number - number with and without decimals
+// Object - everything in JS is an object. Object is unique while all the other types are primitives
+// Boolean - true or false
+// Null - way to say nothing
+// Undefined - way to say nothing
+// Symbol - always gives unique identifier
