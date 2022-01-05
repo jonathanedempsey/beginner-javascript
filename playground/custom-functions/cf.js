8e00a91ba906016cbcba6ae8a9f3ec3cae5e9d02
Jonathan Dempsey <jonathanedempsey@gmail.com>
2022-01-05 16:01:40 +0000

    Functions - Parameters and Arguments

--- playground/custom-functions/cf.js
+++ playground/custom-functions/cf.js
@@ -1,13 +1,12 @@
 // Function Definition
-function calculateBill() {
+function calculateBill(billAmount, taxRate = 0.13) {
     // this is the function body/block
 
     console.log('Running Calculate Bill');
 
     // Scoped variable.
     // This variable is only available within this function
-    const total = 100 * 1.13;
-    // console.log(total);
+    const total = billAmount * (1 + taxRate);
 
     // To use total outside of the function we need to use 'return'
     return total;
@@ -16,7 +15,35 @@ function calculateBill() {
 
 // Function Call
 // We can capture the 'total' variable from calculateBill() in a new variable
-const myTotal = calculateBill();
+const myTotal = calculateBill(100, 0.13);
 
-console.log(`Your Total is ${myTotal}`);
+// Since we have a defaulkt value for taxRate, we can just pass in the billAmount for the same result
+// const myTotal = calculateBill(100);
 
+console.log(`Your Total is $${myTotal}`);
+
+// Function Definition
+function sayHiTo(firstName) {
+    return `Hello ${firstName}`;
+}
+
+const greeting = sayHiTo('Jonno');
+console.log(greeting);
+
+
+// In the two functions below, it is okay to reuse the same parameter names
+// since the paramater name is scoped and can only be used in that specific function
+function doctorize(name) {
+    return `Dr. ${name}`;
+}
+
+function yell(name = 'Silly Goose') {
+    return `HEY ${name.toUpperCase()}`;
+}
+
+// We can also pass functions as arguments in other functions
+console.log(yell(doctorize('Jonno')));
+
+// We can set a default value on a function so if no argument is passed,
+// it will revert to the default value
+console.log(yell())
