aa7ffa695c7a068961964fbb5ba3406225c1455d
Jonathan Dempsey <jonathanedempsey@gmail.com>
2022-01-07 14:16:01 +0000

    Scope
    function and block scope examples

--- playground/scope.js
+++ playground/scope.js
@@ -1,1 +1,48 @@
 console.log('it works!');
+
+// Global variable
+// Accessible from anywhere
+const first = 'jonno';
+
+
+// Function scope
+const age = 100; // global
+function go() {
+    const hair = 'blonde'; // scoped
+}
+
+// Age will print out as it's a global variable
+console.log(age);
+// Hair will return as not defined as it is scoped in the go() function
+console.log(hair);
+
+
+// Block scope
+// As a general rule for function and block scope,
+// any variable created within two curly brackets cannot be accessed outside of those curly brackets
+if ( 1 === 1) {
+    const cool = true;
+}
+console.log(cool); // wont work as cool is block scoped.
+
+// A work around for block scope is to declare the variable outside the block
+// and then update that variable within the block.
+let cool;
+if ( 1 === 1 ) {
+    cool = true;
+}
+
+console.log(cool); // this works!
+
+// You'd probably want to add the above into a function to avoid the global variable
+// Function scope
+function isCool(name) {
+    let cool;
+
+    // Block scope
+    if ( name === 'jonno' ) {
+        cool = true;
+    }
+    console.log(cool);
+    return cool;
+}
